ea8b8dabbcb9a429eca6bc72d1398ff4806b7b24
Mirjami Laiho <mirjami.laiho@tuni.fi>
2023-03-17 16:42:21 +0200

    Move latitude and longitude fetching functions to App.js. Modify the view according to the selection of all cities or a single city.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
+import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import DropDownMenu from "./components/DropDownMenu";
+import CurrentWeather from "./components/CurrentWeather";
+import Forecast from "./components/Forecast";
 
-function App() {
+const App = () => {
+  const [currentLocation, setCurrentLocation] = React.useState("All cities");
+  const locations = ["Espoo", "Jyväskylä", "Kuopio", "Tampere"];
+
+  const getLat = (location) => {
+    switch (location) {
+      case "Espoo":
+        return 60.25;
+      case "Jyväskylä":
+        return 62.2415;
+      case "Kuopio":
+        return 62.8924;
+      case "Tampere":
+        return 61.4991;
+      default:
+        return 60.25;
+    }
+  };
+
+  const getLon = (location) => {
+    switch (location) {
+      case "Espoo":
+        return 24.6667;
+      case "Jyväskylä":
+        return 25.7209;
+      case "Kuopio":
+        return 27.677;
+      case "Tampere":
+        return 23.7871;
+      default:
+        return 24.6667;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <DropDownMenu setCurrentLocation={setCurrentLocation} />
+      {currentLocation === "All cities" && (
+        <div>
+          {locations.map((location, index) => {
+            return (
+              <div key={index}>
+                <CurrentWeather
+                  location={location}
+                  getLat={getLat}
+                  getLon={getLon}
+                />
+                <Forecast location={location} getLat={getLat} getLon={getLon} />
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {currentLocation !== "All cities" && (
+        <div>
+          <CurrentWeather
+            location={currentLocation}
+            getLat={getLat}
+            getLon={getLon}
+          />
+          <Forecast
+            location={currentLocation}
+            getLat={getLat}
+            getLon={getLon}
+          />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
